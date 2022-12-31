6c78d0aafd9cd5fdb799148468d2af87a8dc254e
xonix <xonixx@gmail.com>
2022-12-31 19:13:55 +0200

    Add basic statistics functionality for admin #35 reportStats

--- src/main/java/com/cmlteam/video_reply_telegram_bot2/stat/StatCollector.java
+++ src/main/java/com/cmlteam/video_reply_telegram_bot2/stat/StatCollector.java
@@ -2,8 +2,9 @@ package com.cmlteam.video_reply_telegram_bot2.stat;
 
 import lombok.RequiredArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Duration;
+import java.util.*;
+import java.util.Map.Entry;
 
 @RequiredArgsConstructor
 public class StatCollector {
@@ -19,5 +20,42 @@ public class StatCollector {
       statIntervals.add(statInterval = new StatInterval(intervalNo));
     }
     statInterval.track(key);
+  }
+
+  /**
+   * @param duration time interval to report stat for
+   * @return stat ordered by count desc
+   */
+  public Map<String, Integer> reportStat(Duration duration) {
+    long now = timeProvider.getCurrentTimeMillis();
+    long from = now - duration.toMillis();
+    long fromNo = from / intervalMillis;
+
+    ListIterator<StatInterval> statBackIterator = statIntervals.listIterator(statIntervals.size());
+
+    StatInterval statInterval;
+    Map<String, Integer> summed = new HashMap<>();
+    while (statBackIterator.hasPrevious()
+        && (statInterval = statBackIterator.previous()).intervalNo >= fromNo) {
+      for (Entry<String, Integer> entry : statInterval.counts.entrySet()) {
+        String key = entry.getKey();
+        int value = entry.getValue();
+        summed.put(key, summed.getOrDefault(key, 0) + value);
+      }
+    }
+
+    return sortMapByValueDesc(summed);
+  }
+
+  private static <K, V extends Comparable<? super V>> Map<K, V> sortMapByValueDesc(Map<K, V> map) {
+    List<Entry<K, V>> list = new ArrayList<>(map.entrySet());
+    list.sort(Entry.comparingByValue(Comparator.reverseOrder()));
+
+    Map<K, V> result = new LinkedHashMap<>();
+    for (Entry<K, V> entry : list) {
+      result.put(entry.getKey(), entry.getValue());
+    }
+
+    return result;
   }
 }
--- src/main/java/com/cmlteam/video_reply_telegram_bot2/stat/StatInterval.java
+++ src/main/java/com/cmlteam/video_reply_telegram_bot2/stat/StatInterval.java
@@ -8,7 +8,7 @@ import java.util.concurrent.ConcurrentHashMap;
 @RequiredArgsConstructor
 public class StatInterval {
   final long intervalNo;
-  private final Map<String, Integer> counts = new ConcurrentHashMap<>();
+  final Map<String, Integer> counts = new ConcurrentHashMap<>();
 
   void track(String key) {
     counts.put(key, counts.getOrDefault(key, 0) + 1);
